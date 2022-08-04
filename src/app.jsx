fd4bbca098d55b3cc82571298086b8d85462c84b
Logan Lentz <logan.lentz1@gmail.com>
2022-08-04 00:44:14 -0500

    syntax.. Rookie mistake

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
-import { PhotoCamera } from 'material-ui/icons';
+import { PhotoCamera } from '@material-ui/icons';
 
 const App = () => {
     return (
