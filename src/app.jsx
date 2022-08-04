07a9bf8bc3c5929ad86ba32caf1c67d5ed66918a
Logan Lentz <logan.lentz1@gmail.com>
2022-08-04 17:59:22 -0500

    makeStyles theme

--- src/app.jsx
+++ src/app.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles (( theme ) => ({
+    container: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6)
+        
+    }
+
+}));
+
 
 const App = () => {
+    const classes = useStyles();
     return (
       <>
             <CssBaseline />
@@ -16,7 +28,7 @@ const App = () => {
                 </Toolbar>
             </AppBar>
             <main>
-                <div>
+                <div className='classes.container'>
                     <Container maxWidth="sm" >
                         <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                             Photo Album
