9cd2da992e2291acc752bd9b46cfda1b51d8b72f
Logan Lentz <logan.lentz1@gmail.com>
2022-08-04 17:51:41 -0500

    took stying out

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
 
-import  Button  from '@material-ui/core/Button';
-import  ButtonGroup  from '@material-ui/core/ButtonGroup';
 
 const App = () => {
     return (
@@ -19,20 +17,30 @@ const App = () => {
             </AppBar>
             <main>
                 <div>
-                    <Container maxWidth="sm">
+                    <Container maxWidth="sm" >
                         <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                             Photo Album
                         </Typography>
                         <Typography variant="h5" align="center" color="textSecondary" paragraph>
                             Hello everyone, this is a photo ablum and I'm trying to make this sentence as long as possible to test to see what the sentence looks like on the screen
                         </Typography>
+                        <div>
+                            <Grid container spacing={2} justify="center">
+                                <Grid item>
+                                    <Button variant="contained" color="primary">
+                                        See my photos
+
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="outlined" color="primary">
+                                        Secondary action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
                     </Container>
                 </div>
-                <ButtonGroup variant="outlined" aria-label="outlined button group">
-                    <Button>One</Button>
-                    <Button>Two</Button>
-                    <Button>Three</Button>
-                </ButtonGroup>
             </main>
       </>
     );
