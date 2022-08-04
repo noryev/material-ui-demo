20cf26136369c2e15203fc0788b714452a07a6c9
Logan Lentz <logan.lentz1@gmail.com>
2022-08-04 00:42:33 -0500

    cant resolve material-ui-icons

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { PhotoCamera } from 'material-ui/icons';
 
 const App = () => {
     return (
-        <div>
-            <Typography variant= "h1">Hello, World!</Typography>
-        </div>
-    )
+      <>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <PhotoCamera />
+                    <Typography variant="h6">
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+      </>
+    );
 }
 
 export default App;
