b2e9b06dd31b3d4cefc7d18dc8a9aa3ec540d5f6
Logan Lentz <logan.lentz1@gmail.com>
2022-08-07 04:16:19 -0500

    ipfsContext

---
+++ src/IPFSContext.js
@@ -0,0 +1,41 @@
+import { createContext, useContext, useState } from "react";
+import { NFTStorage, File } from "nft.storage";
+
+const IPFSContext = createContext({});
+
+export function IPFSContextProvider(props) {
+  const [IPFSuploading, setIPFSuploading] = useState(false);
+  const [IPFSerror, setIPFSerror] = useState(null);
+
+  async function IPFSupload(data, file) {
+    try {
+      setIPFSerror(null);
+      setIPFSuploading(true);
+      const client = new NFTStorage({
+        token: process.env.REACT_APP_NFT_STORAGE_KEY
+      });
+      const metadata = await client.store({
+        name: data.name,
+        description: data.description,
+        image: new File([file], file.name, { type: file.type })
+      });
+      console.log(metadata);
+      return metadata.url;
+    } catch (error) {
+      console.error(error);
+      setIPFSerror(error);
+    } finally {
+      setIPFSuploading(false);
+    }
+  }
+
+  return (
+    <IPFSContext.Provider value={{ IPFSuploading, IPFSerror, IPFSupload }}>
+      {props.children}
+    </IPFSContext.Provider>
+  );
+}
+
+export function useIPFS() {
+  return useContext(IPFSContext);
+}
