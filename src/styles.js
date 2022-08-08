1dc652330539ee7e4d2441f109b917ec2e27f7fd
Logan Lentz <logan.lentz1@gmail.com>
2022-08-08 00:57:13 -0500

    photos wont propigate

--- src/styles.js
+++ src/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 // you have this theme object available from MaterialUI- learn more!
 
-const useStyles = makeStyles (( theme) => ({
+const useStyles = makeStyles((theme) => ({
     container: {
         backgroundColor: theme.palette.background.paper,
         padding: theme.spacing(8, 0, 6)
@@ -11,11 +11,9 @@ const useStyles = makeStyles (( theme) => ({
     },
     icon: {
         marginRight: '20px' ,
-
     },
     buttons: {
         marginTop: '40px'
-
     },
     cardGrid: {
         padding: '20px 0'
@@ -27,7 +25,6 @@ const useStyles = makeStyles (( theme) => ({
     },
     CardMedia: {
         paddingTop: '56.25%' // 16:9 aspect ratio
-
     },
     cardContent: {
         flexGrow: 1,
