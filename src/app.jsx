ab25720d3c43f276724b5691c88a88160d19e686
Logan Lentz <logan.lentz1@gmail.com>
2022-08-07 22:46:00 -0500

    ugh css in js is a pain wow

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
-import { makeStyles } from '@material-ui/core/styles';
+import  useStyles  from './styles';
 
-
-const useStyles = makeStyles((theme) => ({
-    container: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6)
-    }
-}));
 
 const App = () => {
     const classes = useStyles();
@@ -26,7 +19,7 @@ const App = () => {
                 </Toolbar>
             </AppBar>
             <main>
-                <div className='classes.container'>
+                <div className={classes.container}>
                     <Container maxWidth="sm">
                         <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                             Photo Album
--- src/styles.js
+++ src/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 // you have this theme object available from MaterialUI- learn more!
 
-const useStyles = makeStyles (( theme ) => ({
+const useStyles = makeStyles (( theme) => ({
     container: {
         backgroundColor: theme.palette.background.paper,
         padding: theme.spacing(8, 0, 6)
